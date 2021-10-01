19483eef7f10a80b1aa8a672935b51291d9323c5
Peter <omgpieisawesome@hotmail.com>
2021-10-01 17:29:26 +0100

    Update servermeta.ts
    Adding placeholder URL for Icons to be visible in the servermeta.json to allow to editing on the instance and easy transfer into distro.json especially with a large amount of packs.

--- src/model/nebula/servermeta.ts
+++ src/model/nebula/servermeta.ts
@@ -24,6 +24,7 @@ export function getDefaultServerMeta(id: string, version: string, options?: Serv
             version: '1.0.0',
             name: `${id} (Minecraft ${version})`,
             description: `${id} Running Minecraft ${version}`,
+            icon: 'https://YourURL.domain/CHANGE_ME.png',
             address: 'localhost:25565',
             discord: {
                 shortId: '<FILL IN OR REMOVE DISCORD OBJECT>',
@@ -64,6 +65,7 @@ export interface ServerMeta {
         version: Server['version']
         name: Server['name']
         description: Server['description']
+        icon: 'https://YourURL.domain/CHANGE_ME.png'
         address: Server['address']
         discord?: Server['discord']
         mainServer: Server['mainServer']
